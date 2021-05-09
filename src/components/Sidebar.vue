bc54c5c3a457a774afe2c57e089aaa99207e1130
Nick Breslin <nickbreslin@gmail.com>
2021-05-09 09:43:50 -0700

    Adding spirit guide

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <SidebarBox title="Action Summary" class="mb-3"
       ><ActionSummary
     /></SidebarBox>
-    <SidebarBox title="Spirit Guide" class="mb-3"><ActionSummary /></SidebarBox>
+    <SidebarBox title="Spirit Guide" class="mb-3"><SpiritGuide /></SidebarBox>
     <SidebarBox title="Effects" class="mb-3"><EffectsSummary /></SidebarBox>
     <SidebarBox title="Action Log" class="mb-3"><ActionLog /></SidebarBox>
   </div>
@@ -12,11 +12,18 @@
 <script>
 import SidebarBox from "@/components/sidebar/SidebarBox";
 import ActionSummary from "@/components/sidebar/ActionSummary";
+import SpiritGuide from "@/components/SpiritGuide";
 import EffectsSummary from "@/components/sidebar/EffectsSummary";
 import ActionLog from "@/components/sidebar/ActionLog";
 export default {
   name: "Sidebar",
-  components: { SidebarBox, ActionSummary, EffectsSummary, ActionLog },
+  components: {
+    SidebarBox,
+    ActionSummary,
+    SpiritGuide,
+    EffectsSummary,
+    ActionLog
+  },
   props: {}
 };
 </script>
