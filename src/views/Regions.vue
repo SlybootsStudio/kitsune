3ce28acaaadd6aac9e1a852a812484ee032469b4
Nick Breslin <nickbreslin@gmail.com>
2021-05-09 11:28:35 -0700

    stubbed out initial views

---
+++ src/views/Regions.vue
@@ -0,0 +1,14 @@
+<template>Skills</template>
+
+<script>
+export default {
+  name: "Skills",
+  components: {},
+  data: function() {
+    return {};
+  },
+  props: {},
+  computed: {},
+  methods: {}
+};
+</script>
