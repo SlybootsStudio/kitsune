f6b919337fb9836e6ede3a6242f0a88f6726af94
Nick Breslin <nickbreslin@gmail.com>
2021-05-08 21:58:37 -0700

    Adding project template

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid mt-5 border-top pt-3 text-small">
+    Footer.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {},
+  computed: {},
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text-small {
+  font-size: 0.8em;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-brand d-flex">
+        <!-- Logo <img
+          src=""
+          alt=""
+          height="30"
+          class="d-inline-flex align-top me-3 pe-3 border-end"
+        /> -->
+        <div class="title me-3 fw-bold">KITSUNE</div>
+        <div class="subtitle">Home</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
