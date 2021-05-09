6b38d9cb8fee6f8a54c1767768f46e1acdbaa242
Nick Breslin <nickbreslin@gmail.com>
2021-05-09 09:35:55 -0700

    Adding in favicon, actionlog and effects summary

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <SidebarBox title="Action Summary"><ActionSummary /></SidebarBox>
+    <SidebarBox title="Action Summary" class="mb-3"
+      ><ActionSummary
+    /></SidebarBox>
+    <SidebarBox title="Spirit Guide" class="mb-3"><ActionSummary /></SidebarBox>
+    <SidebarBox title="Effects" class="mb-3"><EffectsSummary /></SidebarBox>
+    <SidebarBox title="Action Log" class="mb-3"><ActionLog /></SidebarBox>
   </div>
 </template>
 
 <script>
 import SidebarBox from "@/components/SidebarBox";
 import ActionSummary from "@/components/ActionSummary";
+import EffectsSummary from "@/components/EffectsSummary";
+import ActionLog from "@/components/ActionLog";
 export default {
   name: "Sidebar",
-  components: { SidebarBox, ActionSummary },
+  components: { SidebarBox, ActionSummary, EffectsSummary, ActionLog },
   props: {}
 };
 </script>
